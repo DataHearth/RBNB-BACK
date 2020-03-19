757ae0eda1061e289affc70410679dcfe879b7f4
DataHearth <antoine.l@antoine-langlois.net>
2020-03-19 14:40:10 +0100

    update users model

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,11 +1,12 @@
 import * as joi from '@hapi/joi';
 
 export default joi.object().keys({
-  address: joi.string(),
-  // eslint-disable-next-line max-len
-  badges: joi.array().items(joi.string()).default([]), // ? Again, probably could move the badges collection here with verification (limitation)
   firstname: joi.string().required(),
   lastname: joi.string().required(),
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+  badges: joi.array().items(joi.string()).default([]),
+  picture: joi.string().uri().default('http://www.einstein.yu.edu/uploadedImages/PollardLab/PhotoNotAvailable.gif?n=5454'),
+  address: joi.string(),
   phone: joi.string(),
-  picture: joi.string().uri(),
 });
