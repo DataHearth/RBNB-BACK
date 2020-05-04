a12118f60cbb838bb3a53088e3a5005237aa2e02
DataHearth <antoine.l@antoine-langlois.net>
2020-05-04 11:15:10 +0200

    delete useless object

--- src/models/dwellings.ts
+++ src/models/dwellings.ts
@@ -5,13 +5,11 @@ export default joi.object().keys({
   description: joi.string().required(),
   location: joi.string().required(), // ? Probably could move the collection in an object here
   pictures: joi.array().items(joi.string().uri()),
-  technicalSheet: joi.object().keys({
-    price: joi.number().required(),
-    rentalType: joi.string(), // ! Set valid data
-    resident: joi.number().positive().integer().required(),
-    rooms: joi.number().positive().integer().required(),
-    services: joi.array().items(joi.string()).required(),
-    smoking: joi.bool().default(false),
-    type: joi.string(),
-  }),
+  price: joi.number().required(),
+  rentalType: joi.string(), // ! Set valid data
+  resident: joi.number().positive().integer().required(),
+  rooms: joi.number().positive().integer().required(),
+  services: joi.array().items(joi.string()).required(),
+  smoking: joi.bool().default(false),
+  type: joi.string(),
 });
