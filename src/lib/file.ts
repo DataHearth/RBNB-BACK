0e6f4771d31e529af817bed817a77e33a815ffac
DataHearth <antoine.l@antoine-langlois.net>
2020-05-05 19:54:07 +0200

    beautify + add functions

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -1,5 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import {Request, Response, NextFunction} from 'express';
+import * as fs from 'fs';
+import * as path from 'path';
 import firebase from './firebase';
 import logger from './logger';
 
@@ -24,47 +26,83 @@ async function uploadFile(metadata: Metadata) {
     // * const url = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(file[0].name)}?alt=media&token=${generatedUuid}`;
 
     return `https://storage.googleapis.com/${bucket.name}/${file[0].name}`;
-  } catch (gcloudError) {
-    logger.error(gcloudError);
-    return null;
+  } catch (error) {
+    logger.error('Google storage error', {error});
+    throw 'Google storage error';
+  }
+}
+
+function clearTempDirectory() {
+  const temp = path.join(__dirname, '../../../', 'temp');
+
+  if (fs.readdirSync(temp).length !== 0) {
+    logger.debug('Cleaning temp directory...');
+    fs.readdir(temp, (_, files) => {
+      files.forEach((file) => fs.unlink(path.join(temp, file), () => {}));
+    });
+  }
+}
+
+async function deleteUploadedFiles(urls: Array<string>) {
+  try {
+    const bucket = firebase.storage().bucket('gs://rbnb-30af7.appspot.com');
+
+    logger.debug('Deleting files...');
+    await Promise.all(urls.map(async (url) => {
+      const splittedUrl = url.split('/');
+      await bucket.file(`${splittedUrl[splittedUrl.length - 2]}/${splittedUrl[splittedUrl.length - 1]}`).delete();
+    }));
+
+    logger.debug(`${urls.length} files deleted`);
+  } catch (error) {
+    logger.error('Storage deletion error', {error});
+    throw 'Google storage error';
   }
 }
 
 export default async function parseBody(req: Request, res: Response, next: NextFunction) {
-  // eslint-disable-next-line no-nested-ternary
-  const files = req.files ? req.files : req.file ? req.file : null;
   const {body} = req;
   const urls = [];
 
-  if (Array.isArray(files)) {
-    const filenames = [];
-    for (let index = 0; index < files.length; index += 1) {
-      // eslint-disable-next-line no-await-in-loop
-      urls.push(await uploadFile({
-        contentType: files[index].mimetype,
-        path: files[index].path,
+  try {
+    if (Array.isArray(req.files) && req.files.length > 0) {
+      const {files} = req;
+
+      logger.debug(`Uploading ${files.length} file${files.length > 1 ? 's' : ''}...`);
+      await Promise.all(files.map(async (file) => {
+        urls.push(await uploadFile({
+          contentType: file.mimetype,
+          path: file.path,
+        }));
       }));
 
-      filenames.push(files[index].filename);
+      logger.info(`${files.length} files uploaded`);
+
+      body.pictures = urls;
+    } else if (req.file) {
+      const {file} = req;
+      logger.debug('Uploading file...');
+      body.picture = await uploadFile({
+        contentType: <string>file.mimetype,
+        path: <string>file.path,
+      });
+
+      logger.info('File uploaded', {filename: file.filename});
     }
 
-    logger.info(`Uploaded ${files.length} file to originals folder`, {filename: filenames});
-    body.pictures = urls;
-  } else if (files !== null) {
-    body.picture = await uploadFile({
-      contentType: <string>files.mimetype,
-      path: <string>files.path,
-    });
-
-    logger.info('Uploaded 1 file to originals folder', {filename: files.filename});
+    clearTempDirectory();
+  } catch (error) {
+    await deleteUploadedFiles(urls);
+    res.status(500).end();
+    return;
   }
 
-  // eslint-disable-next-line no-restricted-syntax
   for (const [key, value] of Object.entries(body)) {
     try {
       body[key] = JSON.parse(<any>value);
-    // eslint-disable-next-line no-empty
-    } catch (error) {}
+    } catch (error) {
+      // Don't parse the property
+    }
   }
 
   next();
