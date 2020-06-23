dd322d450544c4da0d9a99199551ac4fe3cfc1a4
DataHearth <antoine.l@antoine-langlois.net>
2020-06-23 16:09:58 +0200

    feat(model): removed default image prop, add UID for user creation

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,12 +1,13 @@
 import * as joi from '@hapi/joi';
 
 export default joi.object().keys({
+  uid: joi.string(),
   firstname: joi.string().required(),
   lastname: joi.string().required(),
   email: joi.string().email().required(),
   password: joi.string().required(),
   badges: joi.array().items(joi.string()).default([]),
-  picture: joi.string().uri().default('http://www.einstein.yu.edu/uploadedImages/PollardLab/PhotoNotAvailable.gif?n=5454'),
+  picture: joi.string().uri(),
   address: joi.string(),
   phone: joi.string(),
 });
